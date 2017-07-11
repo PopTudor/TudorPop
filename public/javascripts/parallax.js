35ef1268fbbe363cc89092fa62b8aecf113d00ab
Tudor Pop <tudor14pop@gmail.com>
2017-07-11 22:29:44 +0300

    Improve tabs animation with page change

--- public/javascripts/parallax.js
+++ public/javascripts/parallax.js
@@ -3,6 +3,17 @@
  */
 $(document).ready(function () {
     $('.parallax').parallax();
-    $('ul.tabs').tabs();
+    $('ul.tabs').tabs({swipeable: true});
+    carouselHeight();
     console.log("loaded")
+});
+function carouselHeight() {
+    $('.carousel.tabs-content').each(function () {
+        var newHeight = $(this).find('.carousel-item img').height();
+        var origStyle = $(this).attr('style');
+        $(this).attr('style', origStyle + '; height: ' + newHeight + 'px ');
+    });
+}
+$(window).resize(function () {
+    carouselHeight();
 });
