4b60f6c8ce5bd7413990fac133089dfda55906d7
Tudor Pop <tudor14pop@gmail.com>
2017-07-11 21:52:28 +0300

    Create separate files per tab

--- public/javascripts/parallax.js
+++ public/javascripts/parallax.js
@@ -1,7 +1,8 @@
 /**
  * Created by tudor on 08/07/2017.
  */
-$(document).ready(function(){
+$(document).ready(function () {
     $('.parallax').parallax();
+    $('ul.tabs').tabs();
     console.log("loaded")
 });
--- public/javascripts/tabs_navigation.js
+++
@@ -1,6 +0,0 @@
-/**
- * Created by tudor on 10/07/2017.
- */
-$(document).ready(function(){
-    // $('ul.tabs').tabs({ 'swipeable': true });
-});
