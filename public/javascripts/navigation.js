aa7fdb3e1e54dc3d008ad350dd1a3288df667cce
Tudor Pop <tudor14pop@gmail.com>
2017-07-13 23:41:21 +0300

    Only scroll up on tab click instead of toolbar click

--- public/javascripts/navigation.js
+++ public/javascripts/navigation.js
@@ -1,9 +1,9 @@
 /**
  * Created by tudor on 13/07/2017.
  */
-$("#navigation_tabs").click(function () {
+$(".tab").click(function () {
     $('html, body').animate({
         scrollTop: 0
     }, 350);
-    return false
+    return true
 });
