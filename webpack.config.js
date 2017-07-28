5666fe4fe8eab18e29de21703a4aa83f3d3fea4c
Tudor Pop <tudor14pop@gmail.com>
2017-07-29 00:01:59 +0300

    install react and show hello world

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+var webpack = require('webpack');
+var path = require('path');
+var BUILD_DIR = path.resolve(__dirname, 'src/public');
+var APP_DIR = path.resolve(__dirname, 'src/app');
+
+
+var config = {
+    entry: './public/javascripts/main.js',
+
+    output: {
+        path: '/Users/tudor/IdeaProjects/TudorPop/',
+        filename: 'index.js',
+    },
+
+    devServer: {
+        inline: true,
+        port: 8080
+    },
+
+    module: {
+        loaders: [
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+
+                query: {
+                    presets: ['es2015', 'react']
+                }
+            }
+        ]
+    }
+};
+module.exports = config;
